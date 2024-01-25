cd58eb8b73ba11f51de36efd0e89d88a14486d10
dev-HongCheol <choisieun89@gmail.com>
2024-01-25 13:39:29 +0900

    fix next config

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  // windows standalone config error
+  output: process.platform !== 'win32' ? 'standalone' : undefined,
   webpack(config, { isServer }) {
     // Configures webpack to handle SVG files with SVGR. SVGR optimizes and transforms SVG files
     // into React components. See https://react-svgr.com/docs/next/
